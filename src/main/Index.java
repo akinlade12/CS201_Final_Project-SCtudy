dee2399aee14ba8cc2817b8635f0e8cffe2bd4c0
Toyosi Akinlade <akinlade@usc.edu>
2019-04-12 16:05:56 -0700

    Added comments to search algorithm

--- src/main/Index.java
+++ src/main/Index.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,6 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import main.StudySpace;
 
 /**
  * Servlet implementation class Index
@@ -58,8 +59,12 @@ public class Index extends HttpServlet {
 	    String hoursOpenReq = (String) request.getParameter("hoursOpenBoxForm");
 	    String hoursCloseReq = (String) request.getParameter("hoursCloseBoxForm");
 	    Vector<StudySpace> studySpaces = new Vector<>();
+	    // Following vectors' indices correspond to id of study space in database
+	    // For each study space, determine if study space matches all parameters that the user submitted
 	    Vector<Boolean> allParamsV = new Vector<>();
+	    // For each study space, determine whether there was a required parameter that didn't match with the study space
 	    Vector<Boolean> dontAddV = new Vector<>();
+	    // For each study space, determine the number of parameters match with the user's search
 	    Vector<Integer> numbMatches = new Vector<>();
 	    try {
 	    	Class.forName("com.mysql.cj.jdbc.Driver");
@@ -68,13 +73,19 @@ public class Index extends HttpServlet {
 	    	st = conn.createStatement();
 	    	rs = st.executeQuery("SELECT * from studySpaces");
 	    	while(rs.next()) {
+	    		// changes to true if desired parameter is required but study space does not have parameter
 	    		Boolean dontAdd = false;
+	    		// changes to false if all parameters are not meant
 	    		Boolean allParams = true;
+	    		// if no study spaces return allParams = true then return all spaces that have the highest number of matches
 	    		int numbOfParams = 0;
 	            
+	    		// if user did not select parameter then ignore
 	            if (!seats.equals("Choose...")) {
+	            	// if user selected parameter does not match the study space's parameter, allParams is false
 		            if(!rs.getObject(6).equals(seats)) {
 		            	allParams = false;
+		            	// if they dont match and it is a required parameter, then do not add study spot to vector
 		            	if(seatsReq.equals("true")) {
 		            		dontAdd = true;
 		            	}
@@ -158,22 +169,29 @@ public class Index extends HttpServlet {
 		            	numbOfParams++;
 		            }
 	            }
+	            
 	            numbMatches.add(numbOfParams);
 	            allParamsV.add(allParams);
 	            dontAddV.add(dontAdd);
 	    	}
 	    	
 	    	Boolean completeMatch = false;
+	    	// vector of studySpaceId's that will be submitted to results page
 	    	Vector<Integer> studySpaceIds = new Vector<>();
+	    	// first loop through allParamsV to determine if there are any study spaces that completely match with user's search
 	    	for(int i = 0; i < allParamsV.size(); i++) {
 	    		if(allParamsV.get(i)) {
 	    			studySpaceIds.add(i + 1);
+	    			// if there is a complete match, we will not loop through to determine the study space with the highest number of matches
 	    			completeMatch = true;
 	    		}
 	    	}
+	    	// variable used to get all study spaces with highest number of matched parameters
 	    	int maxMatches = 0;
+	    	// if there is not a study space with a complete match, loop through numbMatches vector to find study spaces with highest number of matches
 	    	if(!completeMatch) {
 	    		for(int i = 0; i < dontAddV.size(); i++) {
+	    			// if the user's required parameter does not match with the study space, we will not add it to the study space vector 
 	    			if(!dontAddV.get(i)) {
 	    				if(numbMatches.get(i) > maxMatches) {
 	    					maxMatches = numbMatches.get(i);
@@ -183,6 +201,7 @@ public class Index extends HttpServlet {
 	    				
 	    			}
 	    		}
+	    		// loop through again to add all study spaces with maxMatches
 	    		for(int i = 0; i < dontAddV.size(); i++) {
 	    			if(!dontAddV.get(i))
 	    			if(maxMatches == numbMatches.get(i)) {
@@ -194,6 +213,7 @@ public class Index extends HttpServlet {
 	    	
 	    	rs2 = st.executeQuery("SELECT * from studySpaces");
 	    	
+	    	// loop through database again and create StudySpace objects for each id in studySpaceIds vector
 	    	while(rs2.next()) {
 	    		
 	    		if(studySpaceIds.contains(rs2.getObject(1))) {
@@ -205,6 +225,7 @@ public class Index extends HttpServlet {
 	    		
 	    	}
 	    	
+	    	// set session variable to studySpaces vector
 	    	session.setAttribute("studySpaces", studySpaces);
 	    	
 	    } catch (SQLException sqle) {
